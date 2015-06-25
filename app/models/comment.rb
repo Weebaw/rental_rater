0eb67565581087140952818314c3b6c6109b2b84
Ben Steinbook <ben.steinbook@gmail.com>
2015-06-25 08:06:35 -0600

    pre presentation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :property
 
-  validates :rating, :numericality => { :greater_than => 1 }
+  validates :rating, :numericality => { :greater_than_or_equal_to => 1 }
 
   validates :rating, :numericality => { :less_than_or_equal_to => 5 }
 
